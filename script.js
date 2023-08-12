087ef0b5f321a7f01ed71ebcbaa545a76e247590
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2023-08-12 12:59:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,23 +32,17 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = ""; // Your Result goes here
+  var codeArr = encodedStr.split("");  // String to Array
+  var decodedArr = []; // Your Result goes here
   // Only change code below this line
-	for(let index=0; index < encodedStr.length; index++){
-		if(encodedStr[index]>="A" && encodedStr[index]<="Z"){
-			decodedArr +=lookup[encodedStr[inex]];
-		}
-		else{
-			decodedArr +=encodedStr[index];
-		}
-	}
 
-  return decodedArr;
+  decodedArr = codeArr.map(function(letter) {
+    if(lookup.hasOwnProperty(letter)) {
+      letter = lookup[letter];
+    }
+    return letter;
+  });
+
+  // Only change code above this line
+  return decodedArr.join(""); // Array to String
 }
-
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
- console.log(rot13("SERR YBIR? NPPVBWBO"));
-
-// Do not change this line
-window.rot13 = rot13;
